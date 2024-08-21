eaa2a02ed24eed29558e71763617e81c1f8b667e
aaron-6960 <138325645+aaron-6960@users.noreply.github.com>
2024-08-22 00:03:47 +0530

    Minor changes

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 
-function Button({children}) {
+function Button({ children }) {
   return (
     <div className="relative grow">
-      <div className="h-16 w-full min-w-80 border-[3px]  border-black bg-slate-50 z-10 flex font-semibold justify-center hover:text-[#8F979F] hover:underline items-center text-xl">
+      <div className="absolute bg-green-500 min-w-80 h-16 w-full left-2 top-2" />
+      <div className="h-16 w-full min-w-80 border-[3px] border-black bg-slate-50 flex font-semibold justify-center hover:text-[#8F979F] hover:underline items-center text-xl relative">
         <p>{children}</p>
       </div>
-      <div className="absolute bg-green-500 min-w-80 h-16 w-full left-2 top-[8px] z-[-1]" />
     </div>
   );
 }
 
-export default Button
+export default Button;
--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -7,16 +7,13 @@ import Button from "./Button";
 
 function Landing() {
   return (
-    <div className="flex flex-col">
-      <img
-        src={background}
-        className="w-screen h-screen absolute z-[-10]"
-        alt="Background"
-      />
-
+    <div
+      style={{ backgroundImage: `url(${background})` }}
+      className="bg-repeat bg-auto flex flex-col min-h-screen"
+    >
       <div className="flex flex-col justify-center pt-6 px-28 gap-5 items-center relative">
-        <div className="flex justify-between w-[85vw] relative ">
-          <img src={FossUnited} className="" alt="Foss United Logo" />
+        <div className="flex justify-between w-[85vw] relative">
+          <img src={FossUnited} alt="Foss United Logo" />
           <div className="flex justify-between font-semibold text-lg items-center gap-10">
             <div className="cursor-pointer hover:text-[#8F979F]">Sponsors</div>
             <div className="cursor-pointer hover:text-[#8F979F]">
@@ -25,7 +22,7 @@ function Landing() {
             <div className="cursor-pointer hover:text-[#8F979F]">FAQ</div>
           </div>
         </div>
-        <div className="bg-green-500 h-[2px] w-[90vw] relative " />
+        <div className="bg-green-500 h-[2px] w-[90vw] relative" />
       </div>
 
       <div className="flex flex-col items-center mt-24">
@@ -38,13 +35,13 @@ function Landing() {
           28-29 OCT 2023 |{" "}
           <a
             className="underline flex"
-            target="blank"
+            target="_blank"
+            rel="noopener noreferrer"
             href="https://www.google.com/maps/place/NIMHANS+Convention+Centre+-+Bengaluru/@12.9414585,77.5922443,17z/data=!4m7!3m6!1s0x3bae15b78a61be57:0xc4498066da178922!8m2!3d12.9431686!4d77.5965084!15sCiROSU1IQU5TIENvbnZlbnRpb24gQ2VudHJlLCBCZW5nYWx1cnUiA4gBAZIBEWNvbnZlbnRpb25fY2VudGVy4AEA!16s%2Fg%2F1tl0vsf_?entry=tts&shorturl=1"
           >
-            {" "}
             NIMHANS Convention Centre, Bengaluru
-            <img src={arrow} />
-          </a>{" "}
+            <img src={arrow} alt="Arrow Icon" />
+          </a>
         </div>
         <div className="w-[80vw] flex gap-10 flex-wrap xl:px-32 2xl:px-40 lg:px-16">
           <div className="flex gap-10 flex-wrap grow">
